be627e0fdc54fbf77d3e22cec1a3dbf6e570db53
MishraAnusha <159795873+MishraAnusha@users.noreply.github.com>
2024-02-23 13:09:45 +0530

    Update urls.py

--- nodes/urls.py
+++ nodes/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     # particular User Nodes
     path('user_nodes/<int:user_id>', views.node_particuler_list, name='user_nodes'),
     path('user_nodes/store_feeds', views.store_feeds, name='user_store_feeds'),
-    path('store-feeds/', views.store_feeds, name='store_feeds'),
     path('user_nodes/get_feeds_chart/<int:node_id>',
          views.get_feeds, name='user_get_feeds'),
     path('user_nodes/get_feeds_table/<int:node_id>',
